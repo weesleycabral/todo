f50430efd66e9d11d3e7ed1f63eaf51cf8728f5a
weesleycabral <weesleylima@hotmail.com>
2024-07-04 15:57:24 -0300

    feat: show todo automatically when add

--- lib/app/ui/pages/home_page.dart
+++ lib/app/ui/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/app/models/todo_model.dart';
 import 'package:todo/app/ui/widgets/my_drawer.dart';
@@ -13,6 +14,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<Todo> todos = <Todo>[];
   bool _newTodoDone = false;
+  late TextEditingController _newTodoController;
+
+  @override
+  void initState() {
+    super.initState();
+    _newTodoController = TextEditingController();
+  }
+
   _addTodo() {
     return showDialog(
       context: context,
@@ -23,35 +32,51 @@ class _HomePageState extends State<HomePage> {
             child: Column(
               children: [
                 TextFormField(
+                  controller: _newTodoController,
                   autofocus: true,
                   decoration: const InputDecoration(hintText: 'Nome da Tarefa'),
-                  onChanged: (value) {},
+                  onChanged: (value) {
+                    _newTodoController.text = value;
+                  },
                 ),
-                const SizedBox(height: 10),
+                const SizedBox(height: 20),
+                // CupertinoSwitch(
+                //   value: _newTodoDone,
+                //   onChanged: (bool value) {
+                //     setState(() {
+                //       _newTodoDone = value;
+                //       (context as Element).markNeedsBuild();
+                //     });
+                //   },
+                // ),
                 CheckboxListTile(
                   title: const Text('Concluído'),
                   value: _newTodoDone,
                   onChanged: (bool? value) {
-                    setState(() {
-                      _newTodoDone = value!;
-                    });
+                    _newTodoDone = value!;
+                    (context as Element).markNeedsBuild();
                   },
                 ),
               ],
             ),
           ),
-          actions: <Widget>[
+          actions: [
             TextButton(
               child: const Text('Cancelar'),
               onPressed: () {
-                Navigator.of(context).pop(); // Fecha o diálogo
+                Navigator.of(context).pop();
               },
             ),
             TextButton(
               child: const Text('Salvar'),
               onPressed: () {
-                // Aqui você pode adicionar a lógica para salvar a tarefa
-                Navigator.of(context).pop(); // Fecha o diálogo
+                String newTodoId = Todo.generateUniqueId(todos);
+                Todo newTodo = Todo(id: newTodoId, title: _newTodoController.text, done: _newTodoDone);
+                print(newTodo.id);
+                setState(() {
+                  todos.add(newTodo);
+                });
+                Navigator.of(context).pop();
               },
             ),
           ],
@@ -64,18 +89,57 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.grey[300],
         title: Text(widget.title),
         centerTitle: true,
       ),
       drawer: const MyDrawer(),
-      body: const Column(
-        children: <Widget>[
-          Text('Hello World!'),
+      body: Column(
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              final todo = todos[index];
+              return Dismissible(
+                key: Key(todo.id!), // Certifique-se de que cada todo tem um ID único.
+                direction: DismissDirection.endToStart, // Permite arrastar apenas da direita para a esquerda.
+                onDismissed: (direction) {
+                  // Remove o item da lista
+                  setState(() {
+                    todos.removeAt(index);
+                  });
+
+                  // Mostra uma Snackbar ou alguma confirmação de que o item foi excluído, se desejado.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      backgroundColor: Colors.red,
+                      content: Text('Todo "${todo.title}" removido'),
+                    ),
+                  );
+                },
+                background: Container(
+                  color: Colors.red,
+                  alignment: Alignment.centerRight,
+                  child: const Padding(
+                    padding: EdgeInsets.only(right: 20.0),
+                    child: Icon(Icons.delete, color: Colors.white),
+                  ),
+                ),
+                child: ListTile(
+                  title: Text(todo.title),
+                  // Adicione aqui outros detalhes do seu Todo, como um ícone de checkbox.
+                ),
+              );
+            },
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _addTodo();
+          _newTodoController.clear();
+          _newTodoDone = false;
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
