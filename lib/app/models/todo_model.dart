2e14a46902e76c07e1ffc8c9e04ccb701484efc5
weesleycabral <weesleylima@hotmail.com>
2024-07-04 15:56:47 -0300

    feat: Add unique ID generation to Todo model

--- lib/app/models/todo_model.dart
+++ lib/app/models/todo_model.dart
@@ -1,9 +1,25 @@
+import 'dart:math';
+
 class Todo {
+  String? id;
   String title;
   bool done;
 
   Todo({
+    required this.id,
     required this.title,
     this.done = false,
   });
+
+  // Método para gerar um ID único
+  static String generateUniqueId(List<Todo> todos) {
+    final random = Random();
+    String generatedId;
+
+    do {
+      generatedId = random.nextInt(10000).toString();
+    } while (todos.any((todo) => todo.id == generatedId));
+
+    return generatedId;
+  }
 }
