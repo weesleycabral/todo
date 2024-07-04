e5bf8071e3c4e41fc46866216b11f9f035465739
weesleycabral <weesleylima@hotmail.com>
2024-07-04 12:41:25 -0300

    feat: create floatingactionbutton to add new todo

--- lib/app/ui/pages/home_page.dart
+++ lib/app/ui/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo/app/models/todo_model.dart';
+import 'package:todo/app/ui/widgets/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -9,17 +11,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Todo> todos = <Todo>[];
+  bool _newTodoDone = false;
+  _addTodo() {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Adicionar Tarefa'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  autofocus: true,
+                  decoration: const InputDecoration(hintText: 'Nome da Tarefa'),
+                  onChanged: (value) {},
+                ),
+                const SizedBox(height: 10),
+                CheckboxListTile(
+                  title: const Text('Concluído'),
+                  value: _newTodoDone,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _newTodoDone = value!;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Fecha o diálogo
+              },
+            ),
+            TextButton(
+              child: const Text('Salvar'),
+              onPressed: () {
+                // Aqui você pode adicionar a lógica para salvar a tarefa
+                Navigator.of(context).pop(); // Fecha o diálogo
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-        ),
-        body: const Column(
-          children: <Widget>[
-            Text('Hello World!'),
-          ],
-        ));
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      drawer: const MyDrawer(),
+      body: const Column(
+        children: <Widget>[
+          Text('Hello World!'),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addTodo();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
